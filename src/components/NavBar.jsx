23266226beb9015c2b6fcb1e449951a2a1971cec
Julian Solla <juliansolla7@gmail.com>
2021-11-16 19:04:05 -0300

    Agregado de navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+
+function NavBar () {    
+    return(
+    <div>
+        <ul class="nav nav-pills nav-fill">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Active</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Much longer nav link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled">Disabled</a>
+            </li>
+        </ul>
+    </div>
+    )
+}
+
+export default NavBar 
